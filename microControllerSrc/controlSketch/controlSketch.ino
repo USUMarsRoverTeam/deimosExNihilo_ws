f6b7e412bb8e390003f933eac77d59d0bdf444f9
Samuel Golding <samuelthegolding@gmail.com>
2026-02-04 19:17:47 -0700

    Re-Un-De Fixing it

--- microControllerSrc/controlSketch/controlSketch.ino
+++ microControllerSrc/controlSketch/controlSketch.ino
@@ -1,17 +1,15 @@
 #include <micro_ros_arduino.h>
-
 #include <stdio.h>
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-
 #include <std_msgs/msg/int32.h>
 #include <std_msgs/msg/float32.h>
 #include <std_msgs/msg/float32_multi_array.h>
+#include <std_msgs/msg/bool.h>
 #include <geometry_msgs/msg/twist.h>
 #include <std_msgs/msg/u_int16_multi_array.h>
-
 #include <PWMServo.h>
 #include <PololuMaestro.h>
 
@@ -23,31 +21,26 @@ rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
 
-// Subscriber Nodes
-rcl_subscription_t sub_twist;   // Drive Motor Controls
-rcl_subscription_t sub_arm;     // Arm Controls
-rcl_subscription_t sub_science; // Auger & Plunger Control
-rcl_subscription_t sub_syringe; // Syringe Motor Control
-rcl_subscription_t sub_carousel;// Carousel Motor Control
-rcl_subscription_t sub_camera;  // Camera Servo Control
+rcl_subscription_t sub_twist;
+rcl_subscription_t sub_arm;
+rcl_subscription_t sub_arm_vel;
+rcl_subscription_t sub_arm_home;
+rcl_subscription_t sub_science;
+rcl_subscription_t sub_syringe;
+rcl_subscription_t sub_carousel;
+rcl_subscription_t sub_camera;
 
-// Publishers 
 rcl_publisher_t pub_temp;
 rcl_publisher_t pub_humidity;
 rcl_publisher_t pub_arm_pos;
-/*Potential Other Publishers:
-  - Odometry?
-  - Heading? If GPS Through Arduino
-  - Linear Actuator Position*/
 
-// Messages
-geometry_msgs__msg__Twist twist_msg;
+geometry_msgs__msg__Twist drivetrain_msg;
 std_msgs__msg__UInt16MultiArray arm_msg, camera_msg;
-std_msgs__msg__Float32MultiArray science_msg;
+std_msgs__msg__Float32MultiArray arm_vel_msg, science_msg;
 std_msgs__msg__Int32 syringe_msg, carousel_msg;
 std_msgs__msg__Float32 temp_msg, humidity_msg;
+std_msgs__msg__Bool arm_home_msg;
 
-// Pinout
 #define PANO_CAM_SERVO_PIN 5
 #define LEFT_DRIVE_PIN 6
 #define RIGHT_DRIVE_PIN 7
@@ -60,12 +53,12 @@ std_msgs__msg__Float32 temp_msg, humidity_msg;
 #define TEMP_PIN A0
 #define HUMIDITY_PIN A1
 
-// System Parameters
-const int stepperDelay = 100; // Corresponds to 5k Steps/Second
+const int stepperDelay = 100;
 const float tempSlope = 0.04;
 const int tempIntercept = -40;
 const float humiditySlope = 0.04;
 const float humidityIntercept = 0.0;
+
 double wheelBase = 0.92;
 double center2edge = wheelBase / 2.0;
 double maxLinearVelocity = 0.5;
@@ -75,22 +68,22 @@ int pwmVals[3] = {1000, 1500, 2000};
 float deadzone = 0.05;
 unsigned long lastCmdTime = 0;
 unsigned long cmdTimeout = 300;
-float temp;
-float humidity;
 
-// Object Initialization
+uint16_t currentArmPos[6] = {6000, 7900, 4300, 6000, 4000, 6000};
+float armServoVels[6] = {0,0,0,0,0,0};
+const uint16_t armServoBounds[6][2] = {
+  {5000,8000},
+  {4400,8000},
+  {4200,7000},
+  {6000,7900},
+  {4000,8000},
+  {3000,9000}
+};
+
 PWMServo leftDrive, rightDrive, augerMotor, plungerMotor, panoCamServo;
 
-// Forward declarations
-void setDrivePWM(double left, double right);
-void setDriveNeutralPWM();
-void moveStepper(int stepPin, int dirPin, int stepCount);
-void calcTempAndHumidity(int tempRaw, int humidityRaw);
-
-// Error Handling
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if ((temp_rc != RCL_RET_OK)){}}
-
 
 void error_loop(){
   while(1){
@@ -99,22 +92,20 @@ void error_loop(){
   }
 }
 
-// Callback Functions For Evaluating Controls
+void setDrivePWM(double left, double right);
+void setDriveNeutralPWM();
+void moveStepper(int stepPin, int dirPin, int stepCount);
 
-// Drivetrain Motor Controls
-void twistCallback(const void * msgin){
+void drivetrainCallback(const void * msgin){
   const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
   lastCmdTime = millis();
 
-  // Dead Zone handling
   double linVel = (abs(msg->linear.x) < deadzone) ? 0.0 : msg->linear.x;
   double spinVel = (abs(msg->angular.z) < deadzone) ? 0.0 : msg->angular.z;
 
-  // Constraining Velocity
   linVel = constrain(linVel, -maxLinearVelocity, maxLinearVelocity);
   spinVel = constrain(spinVel, -maxSpinVelocity, maxSpinVelocity);
 
-  // Splitting into Left and Right Motor Velocities
   double leftDriveVel = linVel - spinVel * center2edge;
   double rightDriveVel = linVel + spinVel * center2edge;
 
@@ -126,6 +117,19 @@ void armCallback(const void * msgin){
   for (int i = 0; i < 6 && i < msg->data.size; i++){
     maestro.setTarget(i, msg->data.data[i]);
   }
+}
+
+void armVelCallback(const void * msgin){
+  const std_msgs__msg__Float32MultiArray * msg = (const std_msgs__msg__Float32MultiArray *)msgin;
+  for (int i = 0; i < 6 && i < msg->data.size; i++){
+    armServoVels[i] = msg->data.data[i];
+  }
+}
+
+void armHomeCallback(const void * msgin){
+  const std_msgs__msg__Bool * msg = (const std_msgs__msg__Bool *)msgin;
+  
+  if (msg->data){maestro.goHome();}
 }
 
 void scienceModuleCallback(const void * msgin){
@@ -153,13 +157,10 @@ void panoCamServoCallback(const void * msgin){
   }
 }
 
-// Helper Functions
 void setDrivePWM(double left, double right){
-  // Constrain Wheel Velocity
   left = constrain(left, -maxWheelVelocity, maxWheelVelocity);
   right = constrain(right, -maxWheelVelocity, maxWheelVelocity);
 
-  // Map to PWM Scale
   int leftPWM = map(left, -maxWheelVelocity, maxWheelVelocity, pwmVals[0], pwmVals[2]);
   int rightPWM = 2 * pwmVals[1] - map(right, -maxWheelVelocity, maxWheelVelocity, pwmVals[0], pwmVals[2]);
   
@@ -173,10 +174,8 @@ void setDriveNeutralPWM() {
 }
 
 void moveStepper(int stepPin, int dirPin, int stepCount){
-  // Set Direction
   digitalWrite(dirPin, stepCount >= 0 ? HIGH : LOW);
 
-  // Create Square Wave Control Signal 
   for (int i = 0; i < abs(stepCount); i++){
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(stepperDelay);
@@ -185,24 +184,41 @@ void moveStepper(int stepPin, int dirPin, int stepCount){
   }
 }
 
-void calcTempAndHumidity(int tempRaw, int humidityRaw){
-  // Calculating Temperature
+void calcTempAndHumidity(int tempRaw, int humidityRaw, float* result){
   float tempVoltage = tempRaw * (3.3/1023.0);
-  temp = (tempVoltage * 1000) * tempSlope + tempIntercept;
+  float tempC = (tempVoltage * 1000) * tempSlope + tempIntercept;
 
-  // Calculating Humidity
   float humidityVoltage = humidityRaw * (3.3/1023.0);
-  humidity = (humidityVoltage * 1000) * humiditySlope + humidityIntercept;
+  float humidity = (humidityVoltage * 1000) * humiditySlope + humidityIntercept;
+  
+  result[0] = tempC;
+  result[1] = humidity;
 }
 
-// Needed for Arduino 
-void setup(){
+void updateArmPos(){
+  static unsigned long lastArmUpdate = 0;
+  
+  unsigned long currentTime = millis();
+  
+  if (lastArmUpdate > 0) {
+    float dt = (currentTime - lastArmUpdate) / 1000.0;
+    
+    for (int i = 0; i < 6; i++){
+      float newPos = currentArmPos[i] + armServoVels[i] * dt;
+      
+      currentArmPos[i] = constrain(newPos, armServoBounds[i][0], armServoBounds[i][1]);
+      
+      maestro.setTarget(i, currentArmPos[i]);
+    }
+  }
+  
+  lastArmUpdate = currentTime;
+}
 
-  // Begin Serial Ports
+void setup(){
   Serial.begin(115200);
   maestroSerial.begin(9600);
 
-  // Attach Motors to Pins
   leftDrive.attach(LEFT_DRIVE_PIN);
   rightDrive.attach(RIGHT_DRIVE_PIN);
   setDriveNeutralPWM();
@@ -213,7 +229,6 @@ void setup(){
   panoCamServo.attach(PANO_CAM_SERVO_PIN);
   panoCamServo.write(67);
 
-  // Attach Stepper Pins
   pinMode(SYRINGE_STEP_PIN, OUTPUT);
   pinMode(SYRINGE_DIR_PIN, OUTPUT);
   pinMode(CAROUSEL_STEP_PIN, OUTPUT);
@@ -223,76 +238,35 @@ void setup(){
   set_microros_transports();
   delay(2000);
 
-  // Creating init_options
   allocator = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // Create ROS Node
   RCCHECK(rclc_node_init_default(&node, "teensy_node", "", &support));
 
-  // Create subscribers
-  RCCHECK(rclc_subscription_init_default(
-    &sub_twist,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
-    "cmd_vel"));
+  RCCHECK(rclc_subscription_init_default(&sub_twist, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "cmd_vel"));
+  RCCHECK(rclc_subscription_init_default(&sub_arm, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16MultiArray), "arm_positions"));
+  RCCHECK(rclc_subscription_init_default(&sub_science, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray), "science_module"));
+  RCCHECK(rclc_subscription_init_default(&sub_syringe, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "syringe_count"));
+  RCCHECK(rclc_subscription_init_default(&sub_carousel, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "carousel_count"));
+  RCCHECK(rclc_subscription_init_default(&sub_camera, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16MultiArray), "camera_servo_angle"));
+  RCCHECK(rclc_subscription_init_default(&sub_arm_vel, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray), "arm_velocities"));
+  RCCHECK(rclc_subscription_init_default(&sub_arm_home, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool), "arm_home"));
 
-  RCCHECK(rclc_subscription_init_default(
-    &sub_arm,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16MultiArray),
-    "arm_positions"));
+  RCCHECK(rclc_publisher_init_default(&pub_temp, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "temperature"));
+  RCCHECK(rclc_publisher_init_default(&pub_humidity, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "humidity"));
 
-  RCCHECK(rclc_subscription_init_default(
-    &sub_science,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
-    "science_module"));
-
-  RCCHECK(rclc_subscription_init_default(
-    &sub_syringe,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "syringe_count"));
-
-  RCCHECK(rclc_subscription_init_default(
-    &sub_carousel,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "carousel_count"));
-
-  RCCHECK(rclc_subscription_init_default(
-    &sub_camera,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt16MultiArray),
-    "camera_servo_angle"));
-
-  // Create publishers
-  RCCHECK(rclc_publisher_init_default(
-    &pub_temp,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-    "temperature"));
-
-  RCCHECK(rclc_publisher_init_default(
-    &pub_humidity,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-    "humidity"));
-
-  // Create executor
-  const int num_handles = 6;  // 6 subscribers
+  const int num_handles = 8;
   RCCHECK(rclc_executor_init(&executor, &support.context, num_handles, &allocator));
   
-  // Create Subscriptions for the Subscribers
-  RCCHECK(rclc_executor_add_subscription(&executor, &sub_twist, &twist_msg, &twistCallback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &sub_twist, &drivetrain_msg, &drivetrainCallback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sub_arm, &arm_msg, &armCallback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sub_science, &science_msg, &scienceModuleCallback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sub_syringe, &syringe_msg, &syringeStepCallback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sub_carousel, &carousel_msg, &carouselStepCallback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &sub_camera, &camera_msg, &panoCamServoCallback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &sub_arm_vel, &arm_vel_msg, &armVelCallback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &sub_arm_home, &arm_home_msg, &armHomeCallback, ON_NEW_DATA));
 
-  // Allocate memory for dynamic arrays
   arm_msg.data.capacity = 6;
   arm_msg.data.size = 0;
   arm_msg.data.data = (uint16_t*) malloc(arm_msg.data.capacity * sizeof(uint16_t));
@@ -304,18 +278,26 @@ void setup(){
   camera_msg.data.capacity = 1;
   camera_msg.data.size = 0;
   camera_msg.data.data = (uint16_t*) malloc(camera_msg.data.capacity * sizeof(uint16_t));
+
+  arm_vel_msg.data.capacity = 6;
+  arm_vel_msg.data.size = 0;
+  arm_vel_msg.data.data = (float*) malloc(arm_vel_msg.data.capacity * sizeof(float));
 }
 
 void loop(){
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 
   if (millis() - lastCmdTime > cmdTimeout){setDriveNeutralPWM();}
-  calcTempAndHumidity(analogRead(TEMP_PIN), analogRead(HUMIDITY_PIN));
-  temp_msg.data = temp;
-  humidity_msg.data = humidity;
+
+  updateArmPos();
+
+  float tempAndHumidity[2];
+  calcTempAndHumidity(analogRead(TEMP_PIN), analogRead(HUMIDITY_PIN), tempAndHumidity);
+  temp_msg.data = tempAndHumidity[0];
+  humidity_msg.data = tempAndHumidity[1];
 
   RCSOFTCHECK(rcl_publish(&pub_temp, &temp_msg, NULL));
   RCSOFTCHECK(rcl_publish(&pub_humidity, &humidity_msg, NULL));
 
-  delay(100); // Makes the system loop at 10 Hz
+  delay(100);
 }
